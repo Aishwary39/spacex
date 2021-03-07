9a16cce0bac45a25417f39db0a2867f919f17d05
Aishwary Mishra <aishwarymishra.id@gmail.com>
2021-03-07 20:59:25 +0530

    README.md

--- src/app/components/launch-list/launch-list.component.ts
+++ src/app/components/launch-list/launch-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SpacexProgramService } from 'src/app/services/spacex-program.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-launch-list',
@@ -19,48 +20,61 @@ export class LaunchListComponent implements OnInit {
   ngOnInit() {
     this.route.queryParams.subscribe((params: any) => {
       this.filters = params;
+      this.params = Object.assign(this.params,this.filters);
       this.getAllLaunches();
     });
   }
 
   getAllLaunches(){
     this.spaceXService.getLaunches().subscribe((res:any)=>{
+      const updatedResults = this.manageResponse(res);
       if(Object.keys(this.filters).length >0){
-        const array = this.filterData(res);
-        console.log(array);
+        const array = this.filterData(updatedResults);
       }else{
-        this.launchProgram = res;
+        this.launchProgram = updatedResults;
       }
-      console.log(this.launchProgram);
     })
   }
 
+  manageResponse(res){
+    for(let i=0; i<res.length;i++){
+      if(res[i].static_fire_date_utc){
+        res[i].years = new Date(res[i].static_fire_date_utc).getFullYear().toString();
+      }else{
+        res[i].years = null;
+      }
+      if(res[i].success != null){
+        res[i].successfulLaunch = res[i].success.toString();
+      }else{
+        res[i].successfulLaunch = null;
+      }
+      if(res[i].cores && res[i].cores.length>0 && res[i].cores[0].landing_attempt){
+        res[i].landingAttempt = res[i].cores[0].landing_attempt.toString();
+      }else{
+        res[i].landingAttempt = null;
+      }
+    }
+    return res;
+  }
+
   filterData(res){
-    const filterKeys = Object.keys(this.filters);
+    const array = _.filter(res,this.filters)
+    this.updateUI();
+    this.launchProgram = array;
+  }
 
-
-
-
-
-    // this.launchProgram = res.filter(item=>{
-
-    //   // let year;
-    //   // if(item.static_fire_date_utc){
-    //   //   year = new Date(item.static_fire_date_utc).getFullYear();
-    //   // }else{
-    //   //   year = null;
-    //   // }
-    //   // let landing;
-    //   // if(item.cores && item.cores[0].landing_attempt){
-    //   //   landing = item.cores[0].landing_attempt;
-    //   // }else{
-    //   //   landing = null;
-    //   // }
-    //   // if(this.filters['years'] && this.filters['launch'] && this.filters['landing']){
-    //   //   return (year && year.toString() == this.filters.years) && (landing && landing.toString().toLowerCase() == this.filters.landing.toLowerCase())
-    //   // }
-    // })
-   // console.log(this.launchProgram);
+  updateUI(){
+    for(let key in this.filters){
+      let val : any = document.getElementById(key);
+      let children : any = val.children;
+      if(children){
+        for(let i=0;i<children.length;i++){
+          if(children[i].innerText.toLowerCase() == this.filters[key]){
+            children[i].classList.add('active');
+          }
+        }
+      }
+    }
   }
 
   checkIfFilterExists(parentElem){
@@ -78,21 +92,25 @@ export class LaunchListComponent implements OnInit {
       let parentElem = event.target.parentElement;
       let elem = event.target;
       if(elem.classList.contains('active')){
+        let id = parentElem.id;
+        delete this.params[id];
         elem.classList.remove('active');
       }else{
         this.checkIfFilterExists(parentElem);
         elem.classList.add('active');
       }
       if(parentElem.classList.contains('years')){
-        textKey = 'static_fire_date_utc';
-      }else if(parentElem.classList.contains('launch')){
-        textKey = 'success';
-      }else if(parentElem.classList.contains('landing')){
-        textKey = 'landing_attempt';
+        textKey = 'years';
+      }else if(parentElem.classList.contains('successfulLaunch')){
+        textKey = 'successfulLaunch';
+      }else if(parentElem.classList.contains('landingAttempt')){
+        textKey = 'landingAttempt';
       }else{
         return;
       }
-      this.params[textKey] = elem.innerText;
+      if(elem.classList.contains('active')){
+        this.params[textKey] = elem.innerText.toLowerCase();
+      }
     }
     this.router.navigate(['/space-programs'], { queryParams: this.params });
   }
